5b534c54ccc06c1725599ff232f18e70541a0d86
Wilhelm Fors <askims_ik94w@hotmail.com>
2017-01-17 14:47:23 +0100

    Initial Commit

---
+++ QuizApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizApp
+//
+//  Created by Wilhelm Fors on 17/01/17.
+//  Copyright © 2017 Wilhelm Fors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuizApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuizApp
+//
+//  Created by Wilhelm Fors on 17/01/17.
+//  Copyright © 2017 Wilhelm Fors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
